b32927305b31e0dff46aba566a3a942bf196c809
Gofer <gofer.ex.studio@gmail.com>
2024-02-18 19:44:55 +0900

    `ec2-userdata.bash`を追加

---
+++ ec2-userdata.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xe
+
+export DOCKER_COMPOSE_VERSION=2.24.6
+
+yum update && yum upgrade
+
+yum install -y git docker
+
+systemctl enable docker && systemctl start docker
+
+usermod -aG docker ec2-user
+
+mkdir -p /usr/local/lib/docker/cli-plugins
+cd /usr/local/lib/docker/cli-plugins
+curl -OL https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64
+mv docker-compose-linux-x86_64 docker-compose
+chmod +x docker-compose
